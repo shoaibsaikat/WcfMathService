c5b88a0f91a7675b930ff8528fc8782878bc03f7
Mina Shoaib Rahman <mirahman@medavante.com>
2017-06-06 15:19:50 +0600

    Fixed rename problem

--- SimpleCalculator/Service References/WcfMathServiceReference/Reference.cs
+++ SimpleCalculator/Service References/WcfMathServiceReference/Reference.cs
@@ -15,7 +15,7 @@ namespace SimpleCalculator.WcfMathServiceReference {
     
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-    [System.Runtime.Serialization.DataContractAttribute(Name="CompositeType", Namespace="http://schemas.datacontract.org/2004/07/WcfMathService")]
+    [System.Runtime.Serialization.DataContractAttribute(Name="CompositeType", Namespace="http://schemas.datacontract.org/2004/07/WcfMathServiceInterface")]
     [System.SerializableAttribute()]
     public partial class CompositeType : object, System.Runtime.Serialization.IExtensibleDataObject, System.ComponentModel.INotifyPropertyChanged {
         
